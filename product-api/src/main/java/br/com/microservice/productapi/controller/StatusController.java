6443412b785414708752464d880e89eba6f65760
Lucas Calebe <lucascalebe07@hotmail.com>
2022-06-15 15:26:51 -0300

    :tada: create product-api microservice

--- product-api/src/main/java/br/com/microservice/productapi/controller/StatusController.java
+++ product-api/src/main/java/br/com/microservice/productapi/controller/StatusController.java
@@ -1,2 +1,24 @@
-package br.com.microservice.productapi.controller;public class StatusController {
+package br.com.microservice.productapi.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/api")
+public class StatusController {
+
+  @GetMapping("/status")
+  public ResponseEntity<HashMap<String, Object>> getApiStatus() {
+    var response = new HashMap<String, Object>();
+    response.put("service", "product-API");
+    response.put("status", "UP");
+    response.put("httpStatus", HttpStatus.OK);
+
+    return ResponseEntity.ok(response);
+  }
 }
