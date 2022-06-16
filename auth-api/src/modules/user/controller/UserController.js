dfb545907fd5015de9e580bba897c40bdcf7f96d
Lucas Calebe <lucascalebe07@hotmail.com>
2022-06-16 12:24:50 -0300

    🚧 getting access token in auth-api

--- auth-api/src/modules/user/controller/UserController.js
+++ auth-api/src/modules/user/controller/UserController.js
@@ -1,6 +1,10 @@
 import UserService from "../service/UserService.js";
 
 class UserController {
+    async getAccessToken(req, res) {
+        let accessToken = await UserService.getAccessToken(req)
+        return res.status(accessToken.status).json(accessToken)
+    }
 
     async findByEmail(req, res) {
         let user = await UserService.findByEmail(req);
--- auth-api/src/modules/user/service/UserService.js
+++ auth-api/src/modules/user/service/UserService.js
@@ -1,6 +1,9 @@
 import UserRepository from "../repository/UserRepository.js"
 import * as httpStatus from "../../../config/constants/httpStatus.js";
 import UserException from "../exception/UserException.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import * as secrets from "../../../config/constants/secrets.js";
 
 class UserService {
 
@@ -35,6 +38,40 @@ class UserService {
     validateUserNotFound(user) {
         if (!user) {
             throw new UserException(httpStatus.BAD_REQUEST, 'User was not found.')
+        }
+    }
+
+    async getAccessToken(req) {
+        try {
+            const {email, password} = req.body;
+            this.validateAccessTokenData(email, password);
+            let user = await UserRepository.findByEmail(email);
+            this.validateUserNotFound(user);
+            await this.validatePassword(password, user.password);
+            let authUser = { id: user.id, name: user.name, email: user.email };
+            const accessToken = jwt.sign({ authUser }, secrets.API_SECRET,{ expiresIn: "1d" });
+
+            return {
+                status: httpStatus.SUCCESS,
+                accessToken,
+            };
+        } catch (err) {
+            return {
+                status: err.status ? err.status : httpStatus.INTERAL_SERVER_ERROR,
+                message: err.message
+            }
+        }
+    }
+
+    validateAccessTokenData(email, password) {
+        if (!email || !password) {
+            throw new UserException(httpStatus.UNAUTHORIZED, "Email and password must be informed.")
+        }
+    }
+
+    async validatePassword(password, hashPassword) {
+        if (!await bcrypt.compare(password, hashPassword)) {
+            throw new UserException(httpStatus.UNAUTHORIZED, "Password doesn't match.")
         }
     }
 }
