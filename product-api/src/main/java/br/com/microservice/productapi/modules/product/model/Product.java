8effc0ee421040c83876bde9b8660b260f971fdb
Lucas Calebe <lucascalebe07@hotmail.com>
2022-06-17 12:57:29 -0300

    :sparkles: add quantity_available field in product

--- product-api/src/main/java/br/com/microservice/productapi/modules/product/model/Product.java
+++ product-api/src/main/java/br/com/microservice/productapi/modules/product/model/Product.java
@@ -34,4 +34,7 @@ public class Product {
   @ManyToOne
   @JoinColumn(name = "FK_CATEGORY", nullable = false)
   private Category category;
+
+  @Column(name = "QUANTITY_AVAILABLE", nullable = false)
+  private Integer quantityAvailable;
 }
