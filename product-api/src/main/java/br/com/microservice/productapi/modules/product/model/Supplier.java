81c44b45d8416201eafec23e2267b5f4b554050d
Lucas Calebe <lucascalebe07@hotmail.com>
2022-06-17 11:59:35 -0300

    :sparkles: Create and map jpa entities (product, category, supplier)

---
+++ product-api/src/main/java/br/com/microservice/productapi/modules/product/model/Supplier.java
@@ -0,0 +1,27 @@
+package br.com.microservice.productapi.modules.product.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "SUPPLIER")
+public class Supplier {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  private Integer id;
+
+  @Column(name = "NAME", nullable = false)
+  private String name;
+}
