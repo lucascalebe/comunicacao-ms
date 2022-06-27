1e65fd0abcdc201a681c4eaf79f6c84f1da4733c
Lucas Calebe <lucascalebe07@hotmail.com>
2022-06-27 12:28:13 -0300

    🚧 create async proccess to receive sales confirmation in sales-api

--- sales-api/src/modules/sales/rabbitmq/salesConfirmationListener.js
+++ sales-api/src/modules/sales/rabbitmq/salesConfirmationListener.js
@@ -1,6 +1,7 @@
 import amqp from "amqplib/callback_api.js";
 import { RABBIT_MQ_URL } from "../../../config/constants/secrets.js";
 import { SALES_CONFIRMATION_QUEUE } from "../../../config/rabbitmq/queue.js";
+import orderService from "../service/orderService.js";
 
 export function listenToSalesConfirmationQueue() {
     amqp.connect(RABBIT_MQ_URL, (error, connection) => {
@@ -13,7 +14,8 @@ export function listenToSalesConfirmationQueue() {
                 throw error;
             }
             channel.consume(SALES_CONFIRMATION_QUEUE, (message) => {
-                console.info(`Recieving message from queue: ${message.content.toString()}`)
+                console.info(`Recieving message from queue: ${message.content.toString()}`);
+                orderService.updateOrder(message);
             }, {noAck: true})
         })
     });
